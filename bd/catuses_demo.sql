826557cec657fffb16c21d9ffe21b2b3bf6d8bb5
daniloobvivas <daniloberroteranv@hotmail.com>
2023-04-25 18:44:29 -0600

    add
    add

--- bd/catuses_demo.sql
+++ bd/catuses_demo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 26-04-2023 a las 02:43:25
+-- Tiempo de generación: 26-04-2023 a las 02:43:38
 -- Versión del servidor: 10.4.27-MariaDB
 -- Versión de PHP: 8.2.0
 

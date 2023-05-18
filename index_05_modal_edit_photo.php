5078f2b7c5566d90909fe1cfea84395ad7f3d975
dbvivas <dvivas@catuses.com>
2023-05-17 20:14:59 -0600

    Update index_05_modal_edit_photo.php
    add

--- index_05_modal_edit_photo.php
+++ index_05_modal_edit_photo.php
@@ -5,7 +5,7 @@
 
 
         <div class="modal-header">
-          <h4 class="modal-title">Editar foto </h4>
+          <h4 class="modal-title">Subir Foto </h4>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">&times;</span>
           </button>
